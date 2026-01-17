408fa6f50c64a69ee62a8c98c64d6246b73aa3d3
Xiuu <Luxiaoran16@icloud.com>
2026-01-17 14:26:57 +0000

    Sat, Jan 17, 2026, 10:26 PM +08:00

--- luogu_test/luogu_test7.c
+++ luogu_test/luogu_test7.c
@@ -7,36 +7,36 @@ int main()
     min = ceil(s / v);
     hour = ceil(min / 60);
     // declaration
-    if hour = 1
+    if (hour = 1)
     {
-        if min <= 50
+        if (min <= 50)
         {
             printf("07:%2f", 60 - min);
         }
-        else if min > 50 && min <= 60
+        else if (min > 50 && min <= 60)
         {
             printf("07:0%1f",60 - min);
         }
     }
     // if hour = 1
-    else if hour <= 8
+    else if (hour <= 8)
     {
-        if min - 60 * hour >= 10
+        if (min - 60 * hour >= 10)
         {
             printf("0%1f:%2f",8 - hour, min - 60 * hour);
         }
         else printf("0%1f:0%1f", 8 - hour, min - 60 * hour);
     }
     // if 1 < hour <= 8
-    else if hour > 8
+    else if (hour > 8)
     {
-        if min - 60 * hour >= 10
+        if (min - 60 * hour >= 10)
         printf("%2f:%2f",24 - (hour - 8), min - 60 * hour);
         else printf("%2f:0%1f",24 - (hour - 8), min - 60 * hour );
     }
-    else if hour > 22
+    else if (hour > 22)
     {
-        if min - 60 * hour >= 10
+        if (min - 60 * hour >= 10)
         {
             printf("0%1f:%2f",24 - (hour - 8), min - 60 * hour)
             else printf ("0%1f:0%1f", 24 - (hour - 8), min - 60 * hour)
