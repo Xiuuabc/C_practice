efdf54034516baa4e0f492397c9f242b6d43246a
Xiuu <Luxiaoran16@icloud.com>
2026-01-17 14:49:52 +0000

    Sat, Jan 17, 2026, 10:49 PM +08:00

--- luogu_test/luogu_test7.c
+++ luogu_test/luogu_test7.c
@@ -12,11 +12,11 @@ int main()
     {
         if (min <= 50)
         {
-            printf("07:%2f", 60 - min);
+            printf("07:%.0f", 60 - min);
         }
         else if (min > 50 && min <= 60)
         {
-            printf("07:0%1f",60 - min);
+            printf("07:0%.0f",60 - min);
         }
     }
     // if hour = 1
@@ -24,25 +24,25 @@ int main()
     {
         if (min - 60 * hour >= 10)
         {
-            printf("0%1f:%2f",8 - hour, min - 60 * hour);
+            printf("0%.0f:%.0f",8 - hour, min - 60 * hour);
         }
-        else printf("0%1f:0%1f", 8 - hour, min - 60 * hour);
+        else printf("0%.0f:0%.0f", 8 - hour, min - 60 * hour);
     }
     // if 1 < hour <= 8
     else if (hour > 8)
     {
         if (min - 60 * hour >= 10)
-        printf("%2f:%2f",24 - (hour - 8), min - 60 * hour);
-        else printf("%2f:0%1f",24 - (hour - 8), min - 60 * hour );
+        printf("%.0f:%.0f",24 - (hour - 8), min - 60 * hour);
+        else printf("%.0f:0%.0f",24 - (hour - 8), min - 60 * hour );
     }
     // if 8< hour <= 22
     else if (hour > 22)
     {
         if (min - 60 * hour >= 10)
         {
-            printf("0%1f:%2f",24 - (hour - 8), min - 60 * hour);
+            printf("0%.0f:%.0f",24 - (hour - 8), min - 60 * hour);
         }
-        else printf ("0%1f:0%1f", 24 - (hour - 8), min - 60 * hour);
+        else printf ("0%.0f:0%.0f", 24 - (hour - 8), min - 60 * hour);
     }
     // if 22 < hour <= 24
     return 0;
