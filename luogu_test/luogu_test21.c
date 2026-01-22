e9742d9dc27ff7842a7e202ad44584896df09449
Xiuu <Luxiaoran16@icloud.com>
2026-01-22 16:09:09 +0800

    Remove compiled binaries

--- luogu_test/luogu_test21.c
+++ luogu_test/luogu_test21.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
-int main()
-{
-    int x, n, m;
-    int week;
-    scanf("%d %n", &x, &n);
-    
+
+int main() {
+    int x, n;
+    scanf("%d %d", &x, &n);
+    int total = 0;
+    for (int i = 0; i < n; i++) {
+        int day = (x + i) % 7;   // 1~5: 周一到周五，6: 周六，0: 周日
+    if (day >= 1 && day <= 5) {
+            total += 250;
+        }
+    }
+    printf("%d\n", total);
+    return 0;
 }
