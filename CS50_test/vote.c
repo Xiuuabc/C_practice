96bef4a9632f965f8debb94c70b1ac5c66cbd09a
Xiuu <Luxiaoran16@icloud.com>
2026-02-13 04:35:54 +0000

    Fri, Feb 13, 2026, 12:35 PM +08:00

--- CS50_test/vote.c
+++ CS50_test/vote.c
@@ -78,12 +78,13 @@ bool vote(string name)
 void print_winner(void)
 {
     // TODO
+    int max_candidates;
     for (int i = 0;i < argc - 1;i++)
     {
         for(int j = 0;j < argc - 1;j++)
         {
             if (candidates[i].votes > candidates[j].votes)
-            
+            max_candidates = candidates[i].votes
         }
     }
     return;
